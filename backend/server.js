f58fa6b614cc13ae83e23c93de4031938b7d30f9
vahoaka <irnarakotoarinirina@gmail.com>
2024-09-22 06:43:22 +0300

    Adding backend folder to the project.

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// Set security HTTP headers
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Api is running...');
+});
+// app.use('/api/v1', mainRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+      .underline.bold
+  )
+);
